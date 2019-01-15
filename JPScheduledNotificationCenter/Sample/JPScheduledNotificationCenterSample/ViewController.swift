ceae66d5dbdec929d7b6c4e71fff05bcf7468cd6
Teddy Newell <teddynewell@icloud.com>
2019-01-14 18:09:54 -0800

    Add empty sample project

---
+++ JPScheduledNotificationCenter/Sample/JPScheduledNotificationCenterSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JPScheduledNotificationCenterSample
+//
+//  Created by Teddy Newell on 1/14/19.
+//  Copyright © 2019 Teddy Newell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
